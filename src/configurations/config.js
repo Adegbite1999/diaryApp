0253144fe81f40366b78301e1d6c76c771dfb3ef
Adegbite Ademola kelvin <Adegbiteademola1999@gmail.com>
2022-01-15 22:53:32 +0100

    chore: fix test env

--- src/configurations/config.js
+++ src/configurations/config.js
@@ -2,11 +2,12 @@ const Pool = require('pg').Pool;
 require('dotenv').config();
 
 
-const isProduction = process.env.NODE_ENV === 'production';
+const isTest = process.env.NODE_ENV === 'test';
+// const isTest = Process.env.NODE_ENV === 'test';
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL :
+    connectionString: isTest ? process.env.DATABASE_TEST :
         connectionString,
      ssl: {
         rejectUnauthorized: false
